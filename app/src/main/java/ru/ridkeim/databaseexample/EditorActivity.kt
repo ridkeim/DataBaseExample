2b82875ce94d78732962031a63067abb6309f247
ridkeim <ridkeim@gmail.com>
2020-12-09 12:17:04 +0300

    реализована вставка новых записей в таблицу

--- app/src/main/java/ru/ridkeim/databaseexample/EditorActivity.kt
+++ app/src/main/java/ru/ridkeim/databaseexample/EditorActivity.kt
@@ -1,21 +1,53 @@
 package ru.ridkeim.databaseexample
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.content_editor.*
+import ru.ridkeim.databaseexample.data.HotelContract
+import ru.ridkeim.databaseexample.data.HotelDbHelper
 
 class EditorActivity : AppCompatActivity() {
+
+    companion object{
+        const val NOT_SAVED_USER_ID = -1
+    }
     private var mGender = 2
+    private var guestId = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
         val supportActionBar = supportActionBar
         Log.d(EditorActivity::class.qualifiedName,"toolbar $supportActionBar")
         setupSpinner()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.editor_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_save -> {
+                saveGuest()
+                finish()
+                return true
+            }
+            R.id.action_delete -> {
+                removeGuest()
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupSpinner() {
@@ -42,5 +74,40 @@ class EditorActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    private fun saveGuest(){
+        if(NOT_SAVED_USER_ID == guestId){
+            insertGuest()
+        }else{
+            updateGuest()
+        }
+    }
+
+    private fun updateGuest() {
+
+    }
+
+    private fun insertGuest(){
+        val name = edit_guest_name.text.toString().trim()
+        val city = edit_guest_city.text.toString().trim()
+        val age = edit_guest_age.text.toString().trim().toInt()
+        val db = HotelDbHelper(this).writableDatabase
+        val values = ContentValues()
+        values.put(HotelContract.GuestEntry.COLUMN_NAME,name)
+        values.put(HotelContract.GuestEntry.COLUMN_CITY,city)
+        values.put(HotelContract.GuestEntry.COLUMN_GENDER,mGender)
+        values.put(HotelContract.GuestEntry.COLUMN_AGE,age)
+        val rowId = db.insert(HotelContract.GuestEntry.TABLE_NAME, null, values)
+        val message = if (rowId == -1L) {
+            "Ошибка при заведении гостя"
+        }else{
+            "Гость заведен под номером $rowId"
+        }
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+
+    private fun removeGuest(){
+        
     }
 }
